dd2a3f5f8328453207a1ff810d7bb8f7a57dc2c4
minhtam <minhtamnghp03@gmail.com>
2025-05-17 19:07:35 +0700

    config/base split server into modular folders

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const config = require('./config');
+require('dotenv').config();
+
+const errorHandler = require('./middlewares/error.middleware.js');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({ origin: config.clientUrl, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ message: 'OK ✅ Server running' });
+});
+
+app.use(errorHandler);
+
+module.exports = app;
---
+++ server/src/config/mail.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  user: process.env.MAIL_USER,
+  pass: process.env.MAIL_PASS,
+  service: 'gmail',
+};
