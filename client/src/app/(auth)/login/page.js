777dfaa50d13c8d1631a9c43d289f224f74ef873
minhtam <minhtamnghp03@gmail.com>
2025-05-19 15:14:21 +0700

    auth pages + dashboard + hooks

---
+++ client/src/app/(auth)/login/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./component/LoginForm";
+
+function page() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default page;
