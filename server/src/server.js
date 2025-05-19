59e0b96bc49403521d4f541c70b2c59174f99967
minhtam <minhtamnghp03@gmail.com>
2025-05-19 15:04:45 +0700

    config/base logo

--- server/src/server.js
+++ server/src/server.js
@@ -3,13 +3,11 @@ const app = require('./app');
 const config = require('./config');
 require('dotenv').config();
 
-console.log('process.env.MONGO_URI:', process.env.MONGO_URI);
 mongoose
   .connect(config.db.uri, config.db.options)
   .then(() => {
     console.log('✅ MongoDB connected');
     app.listen(config.port, () => {
-      console.log(`MONGO_URI: ${config.db.uri}`);
       console.log(`🚀 Server running at http://localhost:${config.port}`);
     });
   })
