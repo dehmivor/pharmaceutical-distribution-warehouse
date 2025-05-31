90cf2a58bb8edeabd4f280c066648a1d5a3b3b67
minhtam <minhtamnghp03@gmail.com>
2025-05-31 19:45:28 +0700

    feat/service integrate mailtrap

---
+++ server/src/controllers/mail.controller.js
@@ -0,0 +1,7 @@
+import { sendEmail } from '../services/mailtrap.service';
+
+sendEmail({
+  to: 'recipient@email.com',
+  subject: 'Test Mailtrap SDK',
+  text: 'Đây là email test gửi từ Mailtrap SDK!',
+});
