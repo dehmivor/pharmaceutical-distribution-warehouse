ce13693e73622697e218178e5fb31dbfad12187c
minhtam <minhtamnghp03@gmail.com>
2025-07-10 01:50:34 +0700

    action button for wm list import ord

--- client/src/menu/warehouse.jsx
+++ client/src/menu/warehouse.jsx
@@ -1,59 +1,96 @@
 const warehouse = {
   id: 'group-manage',
-  title: 'Manage',
+  title: 'Warehouse Management',
   icon: 'IconBrandAsana',
   type: 'group',
   children: [
     {
-      id: 'view-import-orders',
-      title: 'View Import Orders',
-      type: 'item',
-      url: '/warehouse-import-orders',
-      icon: 'IconFileImport'
-    },
-    {
-      id: 'inspection',
-      title: 'Manage Inspections',
+      id: 'import-orders',
+      title: 'Import',
       type: 'collapse',
-      icon: 'IconChecklist',
+      icon: 'IconFileImport',
       children: [
         {
-          id: 'create-inspection',
-          title: 'Create Inspections',
+          id: 'view-import-orders',
+          title: 'Import Orders List',
           type: 'item',
-          url: '/create-inspections',
-          matchPattern: '/create-inspections/*',
-          icon: 'IconMapPin'
+          url: '/wh-import-orders',
+          icon: 'IconList'
         },
         {
-          id: 'approve-inspections',
-          title: 'Approve Inspections',
+          id: 'import-inspections',
+          title: 'Import Inspections',
           type: 'item',
-          url: '/approve-inspections',
-          icon: 'IconBoxMultiple'
+          url: '/create-inspections',
+          icon: 'IconChecklist'
         }
       ]
     },
     {
-      id: 'manage-inventory',
-      title: 'Manage Inventory',
-      type: 'item',
-      url: '/manage-inventory',
-      icon: 'IconBoxMultiple'
+      id: 'export-orders',
+      title: 'Export',
+      type: 'collapse',
+      icon: 'IconFileExport',
+      children: [
+        {
+          id: 'view-export-orders',
+          title: 'Export Orders List',
+          type: 'item',
+          url: '/wh-export-orders',
+          icon: 'IconList'
+        }
+      ]
     },
     {
-      id: 'manage-locations',
-      title: 'Manage Locations',
-      type: 'item',
-      url: '/manage-locations',
-      icon: 'IconMapPin'
+      id: 'inventory-check',
+      title: 'Inventory Check',
+      type: 'collapse',
+      icon: 'IconClipboardCheck',
+      children: [
+        {
+          id: 'inventory-orders',
+          title: 'Inventory Sessions',
+          type: 'item',
+          url: '/wh-inventory-orders',
+          icon: 'IconCalendar'
+        },
+        {
+          id: 'inventory-inspections',
+          title: 'Inventory Inspections',
+          type: 'item',
+          url: '/wh-inventory-inspections',
+          icon: 'IconChecklist'
+        }
+      ]
     },
     {
-      id: 'manage-packages',
-      title: 'Manage Packages',
-      type: 'item',
-      url: '/manage-packages',
-      icon: 'IconPackage'
+      id: 'management',
+      title: 'Management',
+      type: 'collapse',
+      icon: 'IconBriefcase',
+      children: [
+        {
+          id: 'manage-locations',
+          title: 'Location Management',
+          type: 'item',
+          url: '/manage-location',
+          icon: 'IconMapPin'
+        },
+        {
+          id: 'manage-packages',
+          title: 'Package Management',
+          type: 'item',
+          url: '/manage-packages',
+          icon: 'IconPackage'
+        },
+        {
+          id: 'return-orders',
+          title: 'Return Management',
+          type: 'item',
+          url: '/wh-return-orders',
+          icon: 'IconRotate'
+        }
+      ]
     }
   ]
 };
