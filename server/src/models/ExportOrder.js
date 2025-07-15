78ecf8675646b7933e99fa1cb22097ed095775eb
Danglq <danglqhe173350@fpt.edu.vn>
2025-07-16 00:40:49 +0700

    sua conflix

--- server/src/models/ExportOrder.js
+++ server/src/models/ExportOrder.js
@@ -39,35 +39,4 @@ const exportOrderSchema = new mongoose.Schema(
   { timestamps: true }, // Add timestamps for createdAt and updatedAt
 )
 
-const exportOrderSchema = new mongoose.Schema({
-  contract_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Contract',
-    required: [true, 'Contract ID is required'],
-  },
-  warehouse_manager_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  status: {
-    type: String,
-    required: [true, 'Status is required'],
-    enum: {
-      values: Object.values(EXPORT_ORDER_STATUSES),
-      message: `Status must be one of: ${Object.values(EXPORT_ORDER_STATUSES).join(', ')}`,
-    },
-    default: EXPORT_ORDER_STATUSES.DRAFT,
-  },
-  created_by: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Created by is required'],
-  },
-  approval_by: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  details: [exportOrderDetailsSchema],
-});
-
 module.exports = mongoose.model("ExportOrder", exportOrderSchema)
