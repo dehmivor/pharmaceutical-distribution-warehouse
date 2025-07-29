5f11536018bcf99040e11913e5f9f9ff837c9007
minhtam <minhtamnghp03@gmail.com>
2025-07-29 20:20:56 +0700

    detail check order with overview

--- server/src/controllers/inventoryController.js
+++ server/src/controllers/inventoryController.js
@@ -76,12 +76,12 @@ const getCheckOrderById = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(checkOrderId)) {
       return res.status(400).json({
         success: false,
-        message: 'Invalid inspection ID',
+        message: 'Invalid check order ID',
       });
     }
 
-    const inspection = await inventoryService.getCheckOrderById(checkOrderId);
-    if (!inspection) {
+    const checkorder = await inventoryService.getCheckOrderById(checkOrderId);
+    if (!checkorder) {
       return res.status(404).json({
         success: false,
         message: 'Check Order not found',
@@ -90,13 +90,13 @@ const getCheckOrderById = async (req, res) => {
 
     return res.json({
       success: true,
-      data: inspection,
+      data: checkorder,
     });
   } catch (error) {
-    console.error('Error fetching inspection by ID:', error);
+    console.error('Error fetching check order by ID:', error);
     return res.status(500).json({
       success: false,
-      message: 'An error occurred while fetching the inspection',
+      message: 'An error occurred while fetching the check order data',
     });
   }
 };
