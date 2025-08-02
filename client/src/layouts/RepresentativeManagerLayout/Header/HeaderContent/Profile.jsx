46873a0b050cdd6d9a365078fcaef81e18d565d2
minhtam <minhtamnghp03@gmail.com>
2025-08-02 07:25:49 +0700

    profile switch locale

--- client/src/layouts/RepresentativeManagerLayout/Header/HeaderContent/Profile.jsx
+++ client/src/layouts/RepresentativeManagerLayout/Header/HeaderContent/Profile.jsx
@@ -43,7 +43,7 @@ const languageList = [
 
 export default function ProfileSection() {
   const theme = useTheme();
-  const { i18n } = useConfig();
+  const { i18n, setI18n } = useConfig();
   const { user, userRole, isLoading, updateUserRole } = useRole();
 
   const [anchorEl, setAnchorEl] = useState(null);
@@ -82,10 +82,12 @@ export default function ProfileSection() {
 
   const i18nHandler = (event, key) => {
     handleInnerActionClick(event);
-    if (key !== i18n) enqueueSnackbar('Upgrade to pro for language change');
+    if (key !== i18n) {
+      setI18n(key);
+      enqueueSnackbar(`Language changed to ${languageList.find((l) => l.key === key)?.value}`, { variant: 'success' });
+    }
   };
 
-  // Chuẩn bị dữ liệu profile lấy từ context
   const profileData = {
     avatar: { src: user?.avatar || '/assets/images/users/avatar-5.png', size: AvatarSize.XS },
     title: user?.email || 'Email',
