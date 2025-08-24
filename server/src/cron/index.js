be22bb6a92a5efff892f59006b6b80d7bcd8a77a
minhtam <minhtamnghp03@gmail.com>
2025-08-24 15:22:39 +0700

    reminders send mail

--- server/src/cron/index.js
+++ server/src/cron/index.js
@@ -1,9 +1,11 @@
 const startInventoryJob = require('./inventoryJob');
 const { startAITrendsJob } = require('./aiTrendsJob');
+const { startReminderEmailJob } = require('./reminderEmailJob');
 
 function startAllCrons() {
   startInventoryJob();
   startAITrendsJob();
+  startReminderEmailJob();
 }
 
 module.exports = startAllCrons;
